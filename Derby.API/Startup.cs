55aa85844dda9532d1d0fd44a523a951a82f1d3d
Ranim Alayoubi <ranimrose1996@gmail.com>
2022-02-20 21:50:59 +0100

    Update Startup

--- Derby.API/Startup.cs
+++ Derby.API/Startup.cs
@@ -52,23 +52,6 @@ namespace Derby.API
             services.AddTransient<ISqlDataAccess, SqlDataAccess>();
             services.AddTransient<IUserData, UserData>();
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = "JwtBearer";
-                options.DefaultChallengeScheme = "JwtBearer";
-            }).AddJwtBearer("JwtBearer", jwtBearerOptions =>
-            {
-                jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ValidateLifetime = true,
-                    ClockSkew = TimeSpan.FromMinutes(5)
-                };
-            }
-            );
             services.AddSwaggerGen(setup =>
             {
                 setup.SwaggerDoc(
