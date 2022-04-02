d6b48623d39400b77b46f55973bfe79b8e7040f3
Ranim Alayoubi <50117658+Ranim1996@users.noreply.github.com>
2022-04-02 15:09:56 +0200

    Renamed Derby to userService
    * Renamed Derby to userService
    
    Co-authored-by: Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>

---
+++ User.DB/StoredProcedures/GetUserById.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetUserById]
+    @userId nvarchar(128)
+AS
+    set nocount on
+
+    select Id, FirstName, LastName, EmailAddress, CreatedDate 
+    from [User]
+    where Id = @userId
+
+    return 0
---
+++ UserService.DataLibrary/DataAccess/UserData.cs
@@ -0,0 +1,40 @@
+﻿using UserService.DataLibrary.Internal.DataAccess;
+using UserService.DataLibrary.Models;
+using System.Collections.Generic;
+
+namespace UserService.DataLibrary.DataAccess
+{
+    public class UserData : IUserData
+    {
+        private readonly ISqlDataAccess _sql;
+
+        public UserData(ISqlDataAccess sql)
+        {
+            _sql = sql;
+        }
+
+        public void UpdateUser(UserModel user)
+        {
+            _sql.SaveData("dbo.UpdateUser", new { user.Id, user.FirstName, user.LastName, user.EmailAddress }, "UserDB");
+        }
+
+        public List<UserModel> GetUsers()
+        {
+            var output = _sql.LoadData<UserModel, dynamic>("dbo.GetUsers", new {  }, "UserDB");
+
+            return output;
+        }
+
+        public List<UserModel> GetUserById(string id)
+        {
+            var output = _sql.LoadData<UserModel, dynamic>("dbo.GetUserById", new { id }, "UserDB");
+
+            return output;
+        }
+
+        public void DeleteUser(string id)
+        {
+            _sql.SaveData("dbo.DeleteUser", new { id }, "UserDB");
+        }
+    }
+}
