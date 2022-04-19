3b10cae4cc1adcfd84fe4fbf51a24e6f27e107f7
Ranim Alayoubi <50117658+Ranim1996@users.noreply.github.com>
2022-04-19 20:12:42 +0200

    Development (#10)
    * cleanup

---
+++ UserService.DataLibrary/Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace UserService.DataLibrary.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedData { get; set; }
+
+        public UserModel()
+        {
+
+        }
+
+        public UserModel(string id, string firstName, string lastName, string email)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = email;
+        }
+    }
+}
