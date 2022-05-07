8cd3e959df745c816782f724c9b8e0e7a9bc8500
Ranim Alayoubi <ranimrose1996@gmail.com>
2022-05-07 19:48:04 +0200

    Small changes

--- Activity.DB/StoredProcedures/RequestStoredProcedures/UpdateRequest.sql
+++ Activity.DB/StoredProcedures/RequestStoredProcedures/UpdateRequest.sql
@@ -1,6 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[UpdateRequest]
 	@requestId int,
-	@userId nvarchar(128),
 	@requestTitle nvarchar(128),
 	@requestDescription nvarchar(128)
 AS
@@ -8,5 +7,5 @@ begin
 	set nocount on;
 	update [Request]
 	set Title = @requestTitle, Description = @requestDescription
-	where Id = @requestId and UserID = @userId
+	where Id = @requestId
 end
--- ActivityService.API/Startup.cs
+++ ActivityService.API/Startup.cs
@@ -51,6 +51,11 @@ namespace ActivityService.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ActivityService.API v1"));
             }
 
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
