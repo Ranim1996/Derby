46624b4390696cc2b6682763999f674005a6fb2f
Ranim Alayoubi <ranimrose1996@gmail.com>
2022-06-04 16:25:50 +0200

    Implementing the LoadTesting using K6 tool to test performance is done.

--- SearchService.API/Controllers/SearchController.cs
+++ SearchService.API/Controllers/SearchController.cs
@@ -8,7 +8,7 @@ namespace SearchService.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = "Bearer")]
+    //[Authorize(AuthenticationSchemes = "Bearer")]
     public class SearchController : ControllerBase
     {
         private readonly ISearchData _searchData;
